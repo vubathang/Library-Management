549aea0ce8f95cbb352f87dc6c5a13778be88f91
Anhnguyen12062003 <anhnguyen988082@gmail.com>
2023-06-01 15:52:18 +0700

    #4 create-uc-sach

---
+++ GUI/Forms/fCapNhatBook.Designer.cs
@@ -0,0 +1,277 @@
+﻿namespace GUI.Forms
+{
+    partial class fCapNhatBook
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.nudSoLuong = new System.Windows.Forms.NumericUpDown();
+            this.dtpNamXB = new System.Windows.Forms.DateTimePicker();
+            this.txtTheLoai = new System.Windows.Forms.TextBox();
+            this.txtNhaXB = new System.Windows.Forms.TextBox();
+            this.txtTacGia = new System.Windows.Forms.TextBox();
+            this.txtID = new System.Windows.Forms.TextBox();
+            this.txtTenSach = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnLuu = new System.Windows.Forms.Button();
+            this.btnThoat = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nudSoLuong)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.BackColor = System.Drawing.Color.White;
+            this.groupBox1.Controls.Add(this.nudSoLuong);
+            this.groupBox1.Controls.Add(this.dtpNamXB);
+            this.groupBox1.Controls.Add(this.txtTheLoai);
+            this.groupBox1.Controls.Add(this.txtNhaXB);
+            this.groupBox1.Controls.Add(this.txtTacGia);
+            this.groupBox1.Controls.Add(this.txtID);
+            this.groupBox1.Controls.Add(this.txtTenSach);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label8);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.groupBox1.Location = new System.Drawing.Point(13, 13);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(1031, 531);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Thông tin sách";
+            // 
+            // nudSoLuong
+            // 
+            this.nudSoLuong.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.nudSoLuong.Location = new System.Drawing.Point(698, 420);
+            this.nudSoLuong.Name = "nudSoLuong";
+            this.nudSoLuong.Size = new System.Drawing.Size(111, 38);
+            this.nudSoLuong.TabIndex = 7;
+            this.nudSoLuong.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // dtpNamXB
+            // 
+            this.dtpNamXB.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.dtpNamXB.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dtpNamXB.Location = new System.Drawing.Point(698, 67);
+            this.dtpNamXB.Name = "dtpNamXB";
+            this.dtpNamXB.Size = new System.Drawing.Size(212, 38);
+            this.dtpNamXB.TabIndex = 5;
+            // 
+            // txtTheLoai
+            // 
+            this.txtTheLoai.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtTheLoai.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtTheLoai.Location = new System.Drawing.Point(698, 243);
+            this.txtTheLoai.Name = "txtTheLoai";
+            this.txtTheLoai.Size = new System.Drawing.Size(264, 38);
+            this.txtTheLoai.TabIndex = 6;
+            // 
+            // txtNhaXB
+            // 
+            this.txtNhaXB.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtNhaXB.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtNhaXB.Location = new System.Drawing.Point(177, 424);
+            this.txtNhaXB.Name = "txtNhaXB";
+            this.txtNhaXB.Size = new System.Drawing.Size(264, 38);
+            this.txtNhaXB.TabIndex = 4;
+            // 
+            // txtTacGia
+            // 
+            this.txtTacGia.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtTacGia.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtTacGia.Location = new System.Drawing.Point(177, 288);
+            this.txtTacGia.Name = "txtTacGia";
+            this.txtTacGia.Size = new System.Drawing.Size(264, 38);
+            this.txtTacGia.TabIndex = 3;
+            // 
+            // txtID
+            // 
+            this.txtID.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtID.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtID.Location = new System.Drawing.Point(177, 66);
+            this.txtID.Name = "txtID";
+            this.txtID.Size = new System.Drawing.Size(264, 38);
+            this.txtID.TabIndex = 1;
+            // 
+            // txtTenSach
+            // 
+            this.txtTenSach.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtTenSach.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtTenSach.Location = new System.Drawing.Point(177, 164);
+            this.txtTenSach.Name = "txtTenSach";
+            this.txtTenSach.Size = new System.Drawing.Size(264, 38);
+            this.txtTenSach.TabIndex = 2;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(20, 427);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(107, 31);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Nhà XB";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(548, 245);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(110, 31);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Thể loại";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(548, 426);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(120, 31);
+            this.label8.TabIndex = 0;
+            this.label8.Text = "Số lượng";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(548, 73);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(114, 31);
+            this.label7.TabIndex = 0;
+            this.label7.Text = "Năm XB";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(20, 295);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(103, 31);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "Tác giả";
+            // 
+            // label
+            // 
+            this.label.AutoSize = true;
+            this.label.Location = new System.Drawing.Point(20, 73);
+            this.label.Name = "label";
+            this.label.Size = new System.Drawing.Size(42, 31);
+            this.label.TabIndex = 0;
+            this.label.Text = "ID";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(20, 166);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(126, 31);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Tên sách";
+            // 
+            // btnLuu
+            // 
+            this.btnLuu.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.btnLuu.AutoSize = true;
+            this.btnLuu.BackColor = System.Drawing.Color.White;
+            this.btnLuu.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnLuu.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.btnLuu.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.btnLuu.Location = new System.Drawing.Point(685, 578);
+            this.btnLuu.Name = "btnLuu";
+            this.btnLuu.Size = new System.Drawing.Size(125, 56);
+            this.btnLuu.TabIndex = 8;
+            this.btnLuu.Text = "Lưu";
+            this.btnLuu.UseVisualStyleBackColor = false;
+            this.btnLuu.Click += new System.EventHandler(this.btnLuu_Click);
+            // 
+            // btnThoat
+            // 
+            this.btnThoat.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.btnThoat.AutoSize = true;
+            this.btnThoat.BackColor = System.Drawing.Color.White;
+            this.btnThoat.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnThoat.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.btnThoat.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.btnThoat.Location = new System.Drawing.Point(891, 578);
+            this.btnThoat.Name = "btnThoat";
+            this.btnThoat.Size = new System.Drawing.Size(124, 56);
+            this.btnThoat.TabIndex = 9;
+            this.btnThoat.Text = "Thoát";
+            this.btnThoat.UseVisualStyleBackColor = false;
+            this.btnThoat.Click += new System.EventHandler(this.btnThoat_Click);
+            // 
+            // fCapNhat
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.btnLuu;
+            this.ClientSize = new System.Drawing.Size(1056, 661);
+            this.Controls.Add(this.btnThoat);
+            this.Controls.Add(this.btnLuu);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "fCapNhat";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "fCapNhat";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nudSoLuong)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnLuu;
+        private System.Windows.Forms.Button btnThoat;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label5;
+        public System.Windows.Forms.TextBox txtNhaXB;
+        public System.Windows.Forms.TextBox txtTacGia;
+        public System.Windows.Forms.TextBox txtTenSach;
+        public System.Windows.Forms.NumericUpDown nudSoLuong;
+        public System.Windows.Forms.DateTimePicker dtpNamXB;
+        public System.Windows.Forms.TextBox txtTheLoai;
+        public System.Windows.Forms.TextBox txtID;
+        private System.Windows.Forms.Label label;
+    }
+}
--- GUI/UserControls/cSach.cs
+++ GUI/UserControls/cSach.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using GUI.Forms;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,14 +8,82 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using System.Data.SqlClient;
+using DTO;
+using DAL;
 namespace GUI.UserControls
 {
+  
     public partial class cSach : UserControl
     {
+        AccessData accessData = new AccessData();
+
         public cSach()
         {
             InitializeComponent();
         }
+
+        private void cSach_Load(object sender, EventArgs e)
+        {
+            string query = "Select * from Sach";
+            dtgSach.DataSource = accessData.GetTable(query);
+
+            int totalBooks = dtgSach.Rows.Count - 1; //Trả về số dòng trong bảng
+            txbTongSo.Text = totalBooks.ToString();
+        }
+        
+
+
+        private void btnThem_Click(object sender, EventArgs e)
+        {
+            fThemBook f = new fThemBook();
+            f.ShowDialog();
+            cSach_Load (sender, e);
+        }
+       
+
+        private void btnCapNhat_Click(object sender, EventArgs e)
+        {
+            fCapNhatBook f = new fCapNhatBook();
+            int i;
+            i = dtgSach.CurrentRow.Index;
+            f.txtID.Text = dtgSach.Rows[i].Cells[0].Value.ToString();
+            f.txtTenSach.Text = dtgSach.Rows[i].Cells[1].Value.ToString();
+            f.txtTacGia.Text = dtgSach.Rows[i].Cells[2].Value.ToString();
+            f.txtNhaXB.Text = dtgSach.Rows[i].Cells[3].Value.ToString();
+            f.dtpNamXB.Text = dtgSach.Rows[i].Cells[4].Value.ToString();
+            f.txtTheLoai.Text = dtgSach.Rows[i].Cells[5].Value.ToString();
+            f.nudSoLuong.Text = dtgSach.Rows[i].Cells[6].Value.ToString();
+            f.ShowDialog();
+            cSach_Load (sender, e);
+           
+        }
+        private string id ;
+       
+        public string Id { get => id; set => id = value; }
+       
+
+        private void btnXoa_Click(object sender, EventArgs e)
+        {
+            int i;
+
+            i = dtgSach.CurrentRow.Index;
+            Id = dtgSach.Rows[i].Cells[0].Value.ToString();
+            
+          
+            string sqlDelete = "Delete from Sach where id =" + Id;
+            try
+            {
+                accessData.Command(sqlDelete);
+                MessageBox.Show("Xóa thành công!");
+                cSach_Load(sender, e);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                MessageBox.Show("Error" + ex.Message);
+            }
+        }
+        
     }
 }
